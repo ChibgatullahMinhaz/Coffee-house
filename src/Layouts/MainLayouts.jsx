9d3e22dbf01fb7dc4292f9d87d8e92e2e5fbe083
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-05-17 03:40:14 +0600

    add a sample loader

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -1,28 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import Footer from "../components/Footer";
 import ScrollButton from "../components/UI/ScrollButton";
+import AtomicSpinner from 'atomic-spinner'
 
 const MainLaouts = () => {
+  const [routeLoading, setRouteLoading] = useState(true);
+  const location = useLocation();
+
   useEffect(() => {
+    setRouteLoading(true);
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
-    console.log('scroll top');
-  }, []);
+    const timeout = setTimeout(() => {
+      setRouteLoading(false);
+    }, 200);
+    return () => clearTimeout(timeout);
+  }, [location]);
   return (
     <>
-    <ScrollButton></ScrollButton>
-      <header >
+      <ScrollButton></ScrollButton>
+      <header>
         <nav>
           <Navbar></Navbar>
         </nav>
       </header>
 
       <main className="" id="minHight">
-        <Outlet></Outlet>
+        {routeLoading ? <div className="flex justify-center items-center">
+          <AtomicSpinner />
+        </div> : <Outlet></Outlet>}
       </main>
 
       <footer>
